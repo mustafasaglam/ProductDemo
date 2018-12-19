332a8fd823b11023969abfe90aa1dfb8347d3978
ms <ms@MS>
2018-12-19 21:31:57 +0300

    Yeni eklemeler

--- ProductDemo.Admin/Controllers/ProductController.cs
+++ ProductDemo.Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using ProductDemo.Core.Infrastructure;
+﻿using Microsoft.Ajax.Utilities;
+using ProductDemo.Core.Infrastructure;
 using ProductDemo.Data.Model;
 using System;
 using System.Collections.Generic;
@@ -14,10 +15,12 @@ namespace ProductDemo.Admin.Controllers
     {
         private readonly IProductRepository _productRepository;
         private readonly ICategoryRepository _categoryrepository;
-        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        private readonly IProductImageRepository _productImagerepository;
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository,IProductImageRepository productImageRepository)
         {
             _productRepository = productRepository;
             _categoryrepository = categoryRepository;
+            _productImagerepository = productImageRepository;
         }
         // GET: Product
         public ActionResult Index()
@@ -57,6 +60,89 @@ namespace ProductDemo.Admin.Controllers
                 product.ProductImages = new List<ProductImage> { img };
             }
             _productRepository.Insert(product);
+            _productRepository.Save();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var product = _productRepository.GetById(id.Value);
+            if (product==null)
+            {
+                return HttpNotFound();
+            }
+            SetCategoryList(product.ProductId);
+            return View(product);          
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product,HttpPostedFileBase ProductImage)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }            
+            _productRepository.Update(product);
+            _productRepository.Save();
+
+            if (ProductImage==null || ProductImage.ContentLength<=0)
+            {
+                return RedirectToAction("Index");
+            }
+            var img = new ProductImage()
+            {
+                ImageName = Path.GetFileName(ProductImage.FileName),
+                ContentType = ProductImage.ContentType
+
+            };
+            using(var reader=new BinaryReader(ProductImage.InputStream))
+            {
+                img.Content = reader.ReadBytes(ProductImage.ContentLength);
+                img.ProductId = product.ProductId;
+            }
+            var existingImage = _productRepository.GetById(product.ProductId).ProductImages;
+            if (existingImage!=null && existingImage.Count >0)
+            {
+                existingImage.ForEach(x => _productImagerepository.Delete(x.ProductImageId));
+            }
+            _productImagerepository.Insert(img);
+            _productImagerepository.Save();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var product = _productRepository.GetById(id.Value);
+            return View(product);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var product = _productRepository.GetById(id.Value);
+            return View(product);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            _productRepository.Delete(id);
             _productRepository.Save();
             return RedirectToAction("Index");
         }
---
+++ ProductDemo.Admin/Helpers/ImageHelper.cs
@@ -0,0 +1,24 @@
+﻿using ProductDemo.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductDemo.Admin.Helpers
+{
+    public static class ImageHelper
+    {
+        public static IHtmlString Base64Image(this HtmlHelper helper, ProductImage productImage)
+        {
+            var imgString = string.Format(@"<img src='data:{productImage},base64,{1}' />",
+            productImage.ContentType,
+            Convert.ToBase64String(productImage.Content)
+
+            );
+            return new HtmlString(imgString);
+            
+
+        }
+    }
+}
